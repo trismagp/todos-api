e5daab9719ec49ef0ab967369156cd30096b4552
kdju234211 <tris magp>
2018-10-28 07:32:47 +0100

    Add todo show, put and delete routes

--- routes/todos.js
+++ routes/todos.js
@@ -25,5 +25,35 @@ router.post('/', function(req, res){
 });
 
 
+router.get('/:todoId', function(req, res){
+  db.Todo.findById(req.params.todoId)
+  .then(function(todo){
+    res.json(todo);
+  })
+  .catch(function(err){
+    res.send(err);
+  })
+});
+
+router.put('/:todoId', function(req, res){
+  db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+  .then(function(todo){
+    res.json(todo);
+  })
+  .catch(function(err){
+    res.send(err);
+  })
+});
+
+router.delete('/:todoId', function(req, res){
+  db.Todo.remove({_id: req.params.todoId})
+  .then(function(){
+    res.json({message:"deleted"});
+  })
+  .catch(function(err){
+    res.send(err);
+  })
+});
+
 
 module.exports = router;
